663226f63ea6334f584fa756e6243995fad10845
Ghaskata <you@example.com>
2024-02-03 00:30:58 +0530

    auth page

--- src/layouts/auth/AuthMaster.jsx
+++ src/layouts/auth/AuthMaster.jsx
@@ -4,18 +4,13 @@ import IcnGoogle from "../../components/svg/IcnGoogle";
 import { Buttonvariants } from "../../components/ui/Button";
 import { cn } from "../../lib/utils";
 import { Link } from "react-router-dom";
-import LoginModal from "../../components/dash/modal/comman/LoginModal";
 import { ToastCon } from "../../components/common/Toast";
-import RegisterModal from "../../components/dash/modal/comman/RegisterModal";
 
 const AuthMaster = () => {
-  const [loginModalOpen, setloginModalOpen] = useState(false);
-  const [registerModalOpen, setregisterModalOpen] = useState(false);
-
   return (
     <>
-      <div className="w-full bg-white dark:bg-black text-dark dark:text-white grid grid-cols-1 md:grid-cols-2 min-h-screen">
-        <div className="w-full hidden md:flex md:justify-center md:items-center col-span-1">
+      <div className="w-full bg-backgroundv1 text-textPrimary grid grid-cols-1 lg:grid-cols-2 min-h-screen">
+        <div className="w-full hidden lg:flex md:justify-center md:items-center col-span-1">
           <div className="image_wrapper h-full w-full overflow-hidden">
             <img src={img} className="object-cover h-full w-full" alt="Logo" />
           </div>
@@ -24,7 +19,7 @@ const AuthMaster = () => {
         <div className="w-full flex flex-col justify-center items-center text-center px-0 sm:px-4 gap-8 col-span-2 md:col-span-1">
           <h1 className="capitalize text-4xl sm:text-5xl">Happening now</h1>
           <h4 className="text-2xl sm:text-3xl">Join today.</h4>
-          <div className="w-full sm:w-1/2 md:3/4 p-4 sm:p-0">
+          <div className="w-full md:w-1/2 lg:w-3/4 p-4 sm:p-0">
             <button
               className={cn(
                 Buttonvariants({ variant: "black" }),
@@ -33,7 +28,7 @@ const AuthMaster = () => {
               onClick={() => console.log("sign Up with google")}
             >
               <IcnGoogle className="h-5 w-5 md:h-6 md:w-6" />
-              <span className="text-sm text-gray-400">Sign Up With Google</span>
+              <span className="text-base md:text-lg lg:text-xl text-gray-400">Sign Up With Google</span>
             </button>
             <div className="flex flex-row justify-center items-center my-3 mx-5">
               <hr className="w-full border-[1px] border-gray-500/20" />
@@ -45,42 +40,30 @@ const AuthMaster = () => {
                 Buttonvariants({ variant: "blue" }),
                 "gap-2 md:gap-3"
               )}
-              role="button"
-              // to={"/register"}
-              onClick={()=>setregisterModalOpen(true)}
+              to={"/register"}
             >
-              <span className="text-sm">Create Account</span>
+              <span className="text-base md:text-lg lg:text-xl">Create Account</span>
             </Link>
           </div>
 
-          <div className="w-full sm:w-1/2 md:3/4 p-4 sm:p-0 mt-5 flex flex-col items-center sm:items-start">
+          <div className="w-full md:w-1/2 lg:w-3/4 p-4 sm:p-0 mt-5 flex flex-col items-center sm:items-start">
             <h5 className="text-md sm:text-lg mb-3">
               Already have an account ?
             </h5>
             <Link
               className={cn(
                 Buttonvariants({ variant: "black" }),
-                "bg-white dark:bg-black hover:bg-slate-300 dark:hover:bg-white  gap-2 md:gap-3"
+                "bg-backgroundv1 hover:bg-slate-300 dark:hover:bg-white gap-2 md:gap-3"
               )}
-              // to={'/login'}
-              role="button"
-              onClick={() => setloginModalOpen(true)}
+              to={"/login"}
             >
-              <span className="text-sm text-blue-500">Sign In</span>
+              <span className="text-base md:text-lg lg:text-xl text-blue-500">Sign In</span>
             </Link>
           </div>
         </div>
       </div>
 
-      <LoginModal
-        loginModalOpen={loginModalOpen}
-        setloginModalOpen={setloginModalOpen}
-      />
-      <RegisterModal
-        registerModalOpen={registerModalOpen}
-        setregisterModalOpen={setregisterModalOpen}
-      />
-      <ToastCon/>
+      <ToastCon />
     </>
   );
 };
