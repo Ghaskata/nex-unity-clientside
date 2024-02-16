40ecc4f5a0f8d27f3ae949a9f49651be07c90513
Ghaskata <you@example.com>
2024-02-16 23:10:14 +0530

    data and useInput hook

---
+++ src/data/data.js
@@ -0,0 +1,22 @@
+export const FrontNavbarList=[
+    {
+        title:"Home",
+        href:"/",
+    },
+    {
+        title:"About us",
+        href:"/about-us",
+    },
+    {
+        title:"News",
+        href:"/news",
+    },
+    {
+        title:"Events",
+        href:"/events",
+    },
+    {
+        title:"Donation",
+        href:"/donation",
+    }
+]
