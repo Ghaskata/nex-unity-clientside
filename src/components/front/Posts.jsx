56d7605df3e55f5fbe347dde0aa1c740b7bc126f
Ghaskata <you@example.com>
2024-02-25 21:38:08 +0530

    post page

--- src/components/front/Posts.jsx
+++ src/components/front/Posts.jsx
@@ -5,15 +5,19 @@ import Post from "./Post";
 const Posts = () => {
   return (
     <div className="w-full h-full">
-      <div className="flex  py-5 xxl:py-10">
-        <div className="flex-grow w-full p-3 xl:p-5 h-[500%] flex flex-col gap-5">
-          <Post />
-          <Post />
-          <Post />
-          <Post />
+      <div className="flex  py-5 xxl:py-10 h-[calc(100vh-80px)]">
+        <div className="flex-grow w-full p-3 xl:p-5 overflow-y-auto post-scroll">
+          <div className=" w-full flex flex-col gap-5">
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+          </div>
         </div>
-        <div className="p-3 xl:p-5 hidden lg:block relative">
-          <HomeUpcommingEvent />
+        <div className="p-3 xl:p-5 hidden lg:block ">
+          <div className="relative">
+            <HomeUpcommingEvent />
+          </div>
         </div>
       </div>
     </div>
