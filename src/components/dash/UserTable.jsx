9b867c4b1b2f3fcc20c3c2150bdf382b2e05e324
Ghaskata <you@example.com>
2024-03-10 10:45:48 +0530

    dash user

--- src/components/dash/UserTable.jsx
+++ src/components/dash/UserTable.jsx
@@ -1,5 +1,11 @@
 import React, { useEffect, useMemo, useState } from "react";
-import { ArrowLeft, ArrowRight, Pen, Trash2 } from "lucide-react";
+import {
+  ArrowLeft,
+  ArrowRight,
+  CheckCheckIcon,
+  Pen,
+  Trash2,
+} from "lucide-react";
 import {
   Table,
   TableBody,
@@ -14,17 +20,27 @@ import { Button } from "../ui/Button";
 import Input from "../ui/Input";
 import useAxiosPrivate from "../../security/useAxiosPrivate";
 import { useQuery } from "react-query";
-import { COMMUNITY_API_URL } from "../../security/axios";
+import { AUTH_API_URL, COMMUNITY_API_URL } from "../../security/axios";
 import DataLoadingCompo from "../common/DataLoadingCompo";
+import defaultimage from "../../assets/images/customeProfile.png";
+import girl from "../../assets/images/girl1.png";
+import boy from "../../assets/images/boy.png";
+import { CgBoy, CgGirl } from "react-icons/cg";
+import { FaUserXmark, FaUserCheck } from "react-icons/fa6";
+import { Switch } from "../ui/switch";
+import { VscEye } from "react-icons/vsc";
+import StatusFilter from "./userTableFilter/StatusFilter";
+import GenderFilter from "./userTableFilter/GenderFilter";
+import ActiveFilter from "./userTableFilter/ActiveFilter";
+import IsRoot from "./userTableFilter/IsRootFilter";
 
 const UserTable = () => {
-  //   const [page, setpage] = useState(1);
-  //   const [totalPages, settotalPages] = useState(Math.ceil(data.length / 5));
-  //   var indexOfLastItem = page * 5;
-  //   const indexOfFirstItem = indexOfLastItem - 5;
-
   const [selectedStatus, setselectedStatus] = useState("All");
-  const [loading, setloading] = useState(true)
+  const [selectedIsRoot, setselectedIsRoot] = useState("All");
+  const [selectedgender, setselectedgender] = useState("All");
+  const [selectedActive, setselectedActive] = useState("All");
+  const [loading, setloading] = useState(true);
+  const [search, setsearch] = useState("");
 
   const axiosPrivate = useAxiosPrivate();
   const queryKey = useMemo(() => ["users"], []);
@@ -37,7 +53,7 @@ const UserTable = () => {
   } = useQuery(
     queryKey,
     async () => {
-      const response = await axiosPrivate.get(COMMUNITY_API_URL.getAll);
+      const response = await axiosPrivate.get(AUTH_API_URL.getAllUser);
       return response.data.data;
     },
     {
@@ -46,31 +62,101 @@ const UserTable = () => {
     }
   );
 
-  console.log("All users >>", users);
+  const handleStatusChange = (value) => {
+    console.log(value); //true or false
+  };
 
+  // console.log("All users >>", users);
 
+  
+  
+  // const handleMark=()=>{
+  //   if (search.trim() !== "") {
+  //     const highlightText = document.getElementsByClassName("highlightText");
+  //     const regex = new RegExp(`(${search.trim().toLocaleLowerCase()})`, "gi");
 
-  if(isError){
-    return(
+  //     for (let i = 0; i < highlightText.length; i++) {
+  //       const originalText = highlightText[i].textContent;
+  //       const highlightedText = originalText.replace(
+  //         regex,
+  //         (match) => `<mark>${match}</mark>`
+  //       );
+  //       highlightText[i].innerHTML = highlightedText;
+  //     }
+  //   }
+  // }
+  
+  const handleSearch=(e)=>{
+    setsearch(e.target.value)
+    // handleMark()
+  }
+
+  if (isError) {
+    return (
       <div className="w-full h-[400px] flex justify-center items-center">
         <DataLoadingCompo />
         <h2 className="text-textPrimary text-center text-26">
           Network Error !!!!
         </h2>
       </div>
-    )
+    );
   }
 
   if (isLoading) {
-    return <DataLoadingCompo/>
+    return <DataLoadingCompo />;
   }
 
   return (
     <div className="w-full">
+      {selectedStatus === "All" &&
+      selectedActive === "All" &&
+      selectedIsRoot === "All" &&
+      selectedgender === "All" ? (
+        ""
+      ) : (
+        <div>
+          <h3 className="text-14 text-textPrimary pb-1 ps-1">
+            {
+              users
+                ?.filter((item) =>
+                  selectedStatus === "All"
+                    ? item
+                    : selectedStatus === "public"
+                    ? item.isPrivate === false
+                    : item.isPrivate === true
+                )
+                ?.filter((item) =>
+                  selectedgender === "All"
+                    ? item
+                    : selectedgender === "Male"
+                    ? item.gender === 1
+                    : item.gender === 2
+                )
+                ?.filter((item) =>
+                  selectedIsRoot === "All"
+                    ? item
+                    : selectedIsRoot === "Root"
+                    ? item.isRoot === true
+                    : item.isRoot === false
+                )
+                ?.filter((item) =>
+                  selectedActive === "All"
+                    ? item
+                    : selectedActive === "Active"
+                    ? item.active === true
+                    : item.active === false
+                ).length
+            }{" "}
+            user found
+          </h3>
+        </div>
+      )}
       <div className="rounded-xl w-full  text-textPrimary text-center text-12  shadow bg-backgroundv1 border-2 border-backgroundv3">
         <div className="p-5 xxl:p-8 w-full  flex flex-col md:flex-row justify-between items-center gap-5">
           <div className="w-full">
             <Input
+              value={search}
+              onChange={handleSearch}
               className="bg-backgroundv2 focus:outline-none border border-backgroundv3 text-textGray h-10 w-full rounded-lg px-5 text-12"
               placeholder="Search ..."
             />
@@ -79,102 +165,211 @@ const UserTable = () => {
             {/* <StatusFilter
               selectedStatus={selectedStatus}
               setselectedStatus={setselectedStatus}
-            />
-            */}
+            /> */}
           </div>
+        </div>
+        <div className="filter_div w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-3 pb-5 px-5 xxl:px-8">
+          <StatusFilter
+            selectedStatus={selectedStatus}
+            setselectedStatus={setselectedStatus}
+          />
+          <GenderFilter
+            selectedgender={selectedgender}
+            setselectedgender={setselectedgender}
+          />
+          <ActiveFilter
+            selectedActive={selectedActive}
+            setselectedActive={setselectedActive}
+          />
+          <IsRoot
+            selectedIsRoot={selectedIsRoot}
+            setselectedIsRoot={setselectedIsRoot}
+          />
         </div>
         <div className=" p-5 xxl:p-8 text-textPrimary text-center text-12 border-t-2 border-backgroundv3">
           <Table className="border-none w-full min-w-[800px]">
             <TableHeader className=" bg-backgroundv2 text-textPrimary !rounded-lg h-16 xl:h-16  border-none ">
               <TableRow className="py-5 border-none">
-                <TableHead className="text-start text-14 xl:text-16 w-[100px] truncate">
+                <TableHead className="text-start text-14 xl:text-16 max-w-[60px] md:max-w-[100px] xl:max-w-full truncate">
                   Id
                 </TableHead>
                 <TableHead className="text-start text-14 xl:text-16">
-                  Front Image
+                  Profile
                 </TableHead>
                 <TableHead className="text-start text-14 xl:text-16">
-                  Name
+                  First
                 </TableHead>
                 <TableHead className="text-start text-14 xl:text-16">
-                  Description
+                  Middle
                 </TableHead>
                 <TableHead className="text-start text-14 xl:text-16">
-                  Owner
+                  Surname
                 </TableHead>
                 <TableHead className="text-start text-14 xl:text-16">
-                  Members
+                  Email
                 </TableHead>
-                <TableHead className="text-start text-14 xl:text-16">
+                <TableHead className="text-center text-14 xl:text-16">
+                  Gender
+                </TableHead>
+                <TableHead className="text-center text-14 xl:text-16">
                   Status
                 </TableHead>
                 <TableHead className="text-start text-14 xl:text-16">
+                  isRoot
+                </TableHead>
+                <TableHead className="text-start text-14 xl:text-16">
                   CreatedAt
+                </TableHead>
+                <TableHead className="text-center text-14 xl:text-16">
+                  Active
                 </TableHead>
                 <TableHead className="text-center text-14 xl:text-16">
                   Actions
                 </TableHead>
               </TableRow>
             </TableHeader>
-            <TableBody>
+            <TableBody className="">
               {users
+                ?.filter((item) => {
+                  const searchLowerCase = search.trim().toLowerCase();
+                  if (searchLowerCase === "") {
+                    return item;
+                  } else {
+                    return (
+                      item.first_name.toLowerCase().includes(searchLowerCase) ||
+                      item.middle_name.toLowerCase().includes(searchLowerCase) ||
+                      item.surname.toLowerCase().includes(searchLowerCase) ||
+                      item.email.toLowerCase().includes(searchLowerCase)
+                    );
+                  }
+                })
                 ?.filter((item) =>
                   selectedStatus === "All"
                     ? item
                     : selectedStatus === "public"
-                    ? item.isPublic === true
-                    : item.isPublic === false
+                    ? item.isPrivate === false
+                    : item.isPrivate === true
+                )
+                ?.filter((item) =>
+                  selectedgender === "All"
+                    ? item
+                    : selectedgender === "Male"
+                    ? item.gender === 1
+                    : item.gender === 2
+                )
+                ?.filter((item) =>
+                  selectedIsRoot === "All"
+                    ? item
+                    : selectedIsRoot === "Root"
+                    ? item.isRoot === true
+                    : item.isRoot === false
+                )
+                ?.filter((item) =>
+                  selectedActive === "All"
+                    ? item
+                    : selectedActive === "Active"
+                    ? item.active === true
+                    : item.active === false
                 )
                 ?.map((user, index) => (
                   <TableRow
                     key={index}
                     className="border-y-2 border-backgroundv3 first:border-t-0"
                   >
-                    <TableCell className="w-[100px] truncate">
+                    <TableCell className="max-w-[60px] md:max-w-[100px] xl:max-w-full truncate">
                       {user._id}
                     </TableCell>
                     <TableCell>
-                      <div className="image_cntainer w-[70px] h-[70px] overflow-hidden bg-textGray/50 rounded-lg">
+                      <div className="image_cntainer w-[60px] h-[60px] overflow-hidden bg-blueMain rounded-full">
                         <img
-                          src={"/images/p1.png"}
+                          src={
+                            user.profile_pic !== ""
+                              ? `${process.env.REACT_APP_SERVER_IMAGE_PATH}${user.profile_pic}`
+                              : defaultimage
+                          }
                           alt="front_image"
                           className="h-full w-full object-cover object-center"
                         />
                       </div>
                     </TableCell>
                     <TableCell>
-                      <div className="text-start text-16 xl:text-18 font-semibold text-blueMain">
-                        {user.name}
+                      <div className="highlightText text-start text-16 xl:text-18 font-semibold text-textPrimary">
+                        {user.first_name}
                       </div>
                     </TableCell>
                     <TableCell>
-                      <div className="text-start text-12 xl:text-14 text-textGray">
-                        {user.description}
+                      <div className="highlightText text-start text-14 xl:text-16 text-textGray">
+                        {user.middle_name}
                       </div>
                     </TableCell>
-                    <TableCell>{}?</TableCell>
-                    <TableCell>{}no ?</TableCell>
+                    <TableCell>
+                      <div className="highlightText text-start text-14 xl:text-16 text-textGray">
+                        {user.surname}
+                      </div>
+                    </TableCell>
+                    <TableCell>
+                      <div className="highlightText text-start text-14 xl:text-16 text-textPrimary">
+                        {user.email}
+                      </div>
+                    </TableCell>
+                    <TableCell>
+                      {user.gender === 1 ? (
+                        <div className="image_cntainer w-[40px] h-[40px] overflow-hidden">
+                          <img
+                            src={boy}
+                            alt="male"
+                            className="h-full w-full object-cover object-center"
+                          />
+                        </div>
+                      ) : (
+                        <div className="image_cntainer w-[40px] h-[40px] overflow-hidden ">
+                          <img
+                            src={girl}
+                            alt="female"
+                            className="h-full w-full object-cover object-center"
+                          />
+                        </div>
+                      )}
+                    </TableCell>
                     <TableCell>
                       <div className="w-full flex items-center justify-center">
                         <div
-                          className={`capitalize py-1 px-3 rounded-full
+                          className={`capitalize py-1 w-20 rounded-full
                             ${
-                              user.isPublic
+                              user.isPrivate
                                 ? "bg-green-600/30 text-green-700"
                                 : "bg-yellow-400/30 text-yellow-700"
                             }`}
                         >
-                          {user.isPublic ? "public" : "private"}
+                          {user.isPrivate ? "private" : "public"}
                         </div>
+                      </div>
+                    </TableCell>
+                    <TableCell>
+                      <div className="w-full flex items-center justify-center">
+                        {user.isRoot ? (
+                          <FaUserCheck className="h-6 w-6 text-green-600" />
+                        ) : (
+                          <FaUserXmark className="h-6 w-6 text-red-600" />
+                        )}
                       </div>
                     </TableCell>
                     <TableCell>
                       {new Date(user.createdAt).toLocaleDateString()}
                     </TableCell>
                     <TableCell>
+                      <div className="flex justify-center items-center w-full">
+                        <Switch
+                          id="active"
+                          checked={user.active}
+                          onCheckedChange={handleStatusChange}
+                        />
+                      </div>
+                    </TableCell>
+                    <TableCell>
                       <div className="flex items-end justify-end gap-4 px-4">
-                        <button className="text-blue-600">
-                          <Pen className="h-6 w-6" />
+                        <button className="text-green-700">
+                          <VscEye className="h-6 w-6" />
                         </button>
                         <button className="text-red-600">
                           <Trash2 className="h-6 w-6 " />
--- src/pages/dash/Users.jsx
+++ src/pages/dash/Users.jsx
@@ -1,10 +1,56 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 import UserTable from "../../components/dash/UserTable";
+import useAxiosPrivate from "../../security/useAxiosPrivate";
+import { useQuery } from "react-query";
+import { AUTH_API_URL } from "../../security/axios";
+import DataLoadingCompo from "../../components/common/DataLoadingCompo";
+import { formatUserFriendlyCount } from "../../lib/userFriendlyCount";
 
 const Users = () => {
+  const axiosPrivate = useAxiosPrivate();
+  const queryKey = useMemo(() => ["users"], []);
+
+  const {
+    data: users,
+    isLoading,
+    isError,
+    error,
+  } = useQuery(
+    queryKey,
+    async () => {
+      const response = await axiosPrivate.get(AUTH_API_URL.getAllUser);
+      return response.data.data;
+    },
+    {
+      enabled: true,
+      refetchOnWindowFocus: false,
+    }
+  );
+
+  if (isError || error) {
+    return (
+      <div className="w-full h-[400px] flex justify-center items-center">
+        <DataLoadingCompo />
+        <h2 className="text-textPrimary text-center text-26">
+          Network Error !!!!
+        </h2>
+      </div>
+    );
+  }
+
+  if (isLoading) {
+    return <DataLoadingCompo />;
+  }
+
   return (
     <div className="dash h-full min-h-screen w-full bg-backgroundv2 transition-all duration-200 ease-in-out p-8 container">
       <h3 className="text-28 lg:text-32 text-textPrimary">Users</h3>
+      {users && (
+        <h5 className="text-12 lg:text-16 text-textGray">
+          Total {formatUserFriendlyCount(users.length)} Users
+        </h5>
+      )}
+
       <div className="py-5 lg:py-8">
         <UserTable />
       </div>
